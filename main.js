e2c12a1794a61835addd558add77dee9475b1af9
jfranquis <janainafranquis@hotmail.com>
2022-05-16 21:09:32 -0300

    Padronizando estruturas, Andicionando Âncoras, Evento Clique Menu, img dentistas

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ function openMenu () {
 function closeMenu () {
     document.body.classList.remove('menu-expanded')
 }
+
+
+
+
+ScrollReveal({  
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+
+}).reveal(`
+#home,
+#home img, 
+#home .statistic, 
+#services, 
+#services header, 
+#services .card,
+#about,
+#about header,
+#about content`);
